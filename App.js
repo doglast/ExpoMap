d45c159053e984121043fc8e4e349558f7ec4c3b
Doglast <des212425@gmail.com>
2021-09-07 21:13:25 -0300

    .

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import MapScreen from './src/screen/MapScreen';
